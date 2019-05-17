491a28fecb70185f8e67dcb20b78dff50d81ccba
Sam <sdfereday@gmail.com>
2019-05-17 15:12:41 +0100

    Tiny modification to logic runners

--- Assets/Src/Modules/Animator/AnimatorLogicManager.cs
+++ Assets/Src/Modules/Animator/AnimatorLogicManager.cs
@@ -7,13 +7,13 @@ using System.Linq;
 public static class Logic
 {
     // Floats
-    public static bool FloatEqual(float a, float b) => a == b;
+    public static bool FloatEqual(float a, float b) => a.Equals(b);
     public static bool FloatGreaterThan(float a, float b) => a > b;
     public static bool FloatLessThan(float a, float b) => a < b;
 
     // Bools
-    public static bool BoolTrue(bool a, bool b) => a == b;
-    public static bool BoolFalse(bool a, bool b) => a != b;
+    public static bool BoolTrue(bool a, bool b) => a.Equals(b);
+    public static bool BoolFalse(bool a, bool b) => !a.Equals(b);
 }
 
 // Conditions
